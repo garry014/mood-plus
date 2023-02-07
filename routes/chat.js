cd79db4c4c4d822511745fcd80e8671e8c4ceb48
staaccceyyyy <76150599+staaccceyyyy@users.noreply.github.com>
2023-02-07 16:48:26 +0800

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -1,13 +1,17 @@
 // Handlebars Helpers
 const alertMessage = require('../helpers/messenger');
+const Chat = require('../models/Chat');
 
 // Other Requires
 const express = require('express');
 const router = express.Router();
 const request = require('request');
 
-router.get('/', (req, res) => {
-    res.render('chat/chat', { title: "Create new thread" })
+router.get('/', async (req, res) => {
+    console.log("here")
+    console.log(req.user)
+    res.render('chat/chat', { title: "Chat", user: req.user })
+
 });
 
 module.exports = router;
